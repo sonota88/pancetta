47ccfdb52eb7c68b136958731247ddb07da58153
_user_ <user@example.com _user_>
2018-09-06 07:20:55 +0900

    wip  y564537u65

--- lib/pancetta/formatter/default.rb
+++ lib/pancetta/formatter/default.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module Pancetta
   module Formatter
     class Default
@@ -13,28 +11,10 @@ module Pancetta
           lines += format_block(block)
         end
 
-        lines << "----"
-        lines << "%d files, %d issues" % [
-          blocks.size,
-          num_issues(blocks)
-        ]
-
-        lines.map{ |line| line + "\n" }.join("")
+        lines.map { |line| line + "\n" }.join('')
       end
 
       private
-
-      def num_issues(blocks)
-        n = 0
-
-        blocks.each do |block|
-          block.issues.each do |issue|
-            n += 1
-          end
-        end
-
-        n
-      end
 
       def format_block(block)
         lines = []

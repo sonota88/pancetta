08818050f0ca8ad63f81b394e7e81bc9f60d94e4
sonota88 <yosiot8753@gmail.com>
2021-08-24 07:25:52 +0900

    2021-08-24 07:25:52 standardrb (cli.rb)

--- lib/pancetta/cli.rb
+++ lib/pancetta/cli.rb
@@ -3,10 +3,17 @@ require "yaml"
 module Pancetta
   module Cli
     def self.run(args)
-      conf = YAML.load(File.read("checkstyle.yaml"))
-      p conf
-      system("java", "-jar", conf["jar_path"], "-c", conf["conf_path"], "src/")
-      puts "TODO"
+      linter_type = args.shift
+      case linter_type
+      when "checkstyle"
+        conf = YAML.load(File.read("checkstyle.yaml"))
+        p conf
+        system("java", "-jar", conf["jar_path"], "-c", conf["conf_path"], "src/")
+      when "standardrb"
+        system "bundle exec standardrb"
+      else
+        raise "not supported"
+      end
     end
   end
 end

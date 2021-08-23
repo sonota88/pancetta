51f33f80d06c306bf06ac006e15c30fedb6318b3
sonota88 <yosiot8753@gmail.com>
2021-08-24 07:12:47 +0900

    Run checkstyle (cli.rb)

--- lib/pancetta/cli.rb
+++ lib/pancetta/cli.rb
@@ -5,6 +5,7 @@ module Pancetta
     def self.run(args)
       conf = YAML.load(File.read("checkstyle.yaml"))
       p conf
+      system("java", "-jar", conf["jar_path"], "-c", conf["conf_path"], "src/")
       puts "TODO"
     end
   end

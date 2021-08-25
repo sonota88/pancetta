a999e173ae1821552a5e358156b48f548f74986d
sonota88 <yosiot8753@gmail.com>
2021-08-26 08:04:23 +0900

    Add Pancetta::Block

---
+++ lib/pancetta/block.rb
@@ -0,0 +1,38 @@
+module Pancetta
+  class Block
+    attr_reader :path, :issues
+
+    def initialize(path, issues)
+      @path = path
+      @issues = issues
+    end
+
+    def self.from_plain(plain)
+      path = plain['path']
+
+      issues = plain['issues']
+        .map { |plain_issue|
+          Issue.from_plain(plain_issue)
+        }
+
+      Block.new(path, issues)
+    end
+
+    def to_plain
+      {
+        'path' => @path,
+        'issues' => @issues.map(&:to_plain)
+      }
+    end
+
+    def to_json(state)
+      JSON.generate(to_plain, state)
+    end
+
+    def each_issue
+      @issues.each do |issue|
+        yield issue
+      end
+    end
+  end
+end
